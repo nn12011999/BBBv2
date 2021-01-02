b4d9740e55c93bcd8df923540e840a896f965f88
nn <nn12011999@gmail.com>
2021-01-02 14:47:57 +0700

    8

--- BBBv2/BBB.Data/Migrations/20210102074747_EditTable_Comment1.Designer.cs
+++ BBBv2/BBB.Data/Migrations/20210102074747_EditTable_Comment1.Designer.cs
@@ -4,14 +4,16 @@ using BBB.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BBB.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210102074747_EditTable_Comment1")]
+    partial class EditTable_Comment1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BBBv2/BBB.Data/Migrations/20210102074747_EditTable_Comment1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BBB.Data.Migrations
+{
+    public partial class EditTable_Comment1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeStamp",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeStamp",
+                table: "Comments");
+        }
+    }
+}
