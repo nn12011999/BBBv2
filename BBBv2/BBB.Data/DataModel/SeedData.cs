353f2bc373dbda223820a0f9ab0554ae549b106d
nn <nn12011999@gmail.com>
2021-01-02 17:20:40 +0700

    22

--- BBBv2/BBB.Data/DataModel/SeedData.cs
+++ BBBv2/BBB.Data/DataModel/SeedData.cs
@@ -29,18 +29,38 @@ namespace BBB.Data.DataModel
                 {
                     Name = "News",
                     ParentId = null,
-                    Editable = false
+                    Editable = false,
+                    Slug = "News-1"
                 };
 
                 var blogs = new Category
                 {
                     Name = "Blogs",
                     ParentId = null,
-                    Editable = false
+                    Editable = false,
+                    Slug = "Blogs-2"
+                };
+
+                var web_info = new Category
+                {
+                    Name = "WebInfo",
+                    ParentId = null,
+                    Editable = false,
+                    Slug = "WebInfo-3"
+                };
+
+                var video = new Category
+                {
+                    Name = "Video",
+                    ParentId = null,
+                    Editable = false,
+                    Slug = "Video-4"
                 };
 
                 context.Categories.Add(news);
                 context.Categories.Add(blogs);
+                context.Categories.Add(web_info);
+                context.Categories.Add(video);
                 context.SaveChanges();
             }
         }
